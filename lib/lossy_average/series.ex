a989447fda767a67a1d6c4fda01c49b2dba27f7d
James Bowers <james.bowers@bbc.co.uk>
2020-12-31 13:50:46 +0000

    rows that should change based off how many times tick/1 is called

--- lib/lossy_average/series.ex
+++ lib/lossy_average/series.ex
@@ -2,32 +2,42 @@ defmodule LossyAverage.Series do
   @doc """
   Called once per minute.
   """
-
-  alias LossyAverage.Math
   alias LossyAverage.Series.Stage
 
   @init_sum 0
   @init_count 0
+  @stage_size 5
 
   def init do
-    [
-      {0, 0},
-      [nil, nil, nil, nil, nil],
-      [nil, nil, nil, nil, nil]
-    ]
+    {0,
+     [
+       {0, 0},
+       [nil, nil, nil, nil, nil],
+       [nil, nil, nil, nil, nil]
+     ]}
   end
 
-  def tick(series, tick_stages) do
+  def tick({tick_counter, series}) do
+    tick_stages = which_stages(tick_counter)
+
     push_average_to_store(series, tick_stages)
     |> reset_first_minute()
+    |> increment_tick_counter(tick_counter)
   end
+
+  defp increment_tick_counter(series, tick_counter), do: {tick_counter + 1, series}
+
+  defp which_stages(counter) when rem(counter, @stage_size) == 0 and counter >= @stage_size do
+    [2, 1]
+  end
+
+  defp which_stages(_counter), do: [1]
 
   defp push_average_to_store(
          series,
          tick_stages
        ) do
     tick_stages
-    |> Math.sort_descending()
     |> Enum.reduce(series, &calc_new_average/2)
   end
 
--- test/lossy_average/series_test.exs
+++ test/lossy_average/series_test.exs
@@ -2,83 +2,77 @@ defmodule LossyAverage.SeriesTest do
   use ExUnit.Case
   alias LossyAverage.Series
 
-  setup do
-    %{
-      init_series: LossyAverage.Series.init()
-    }
-  end
-
-  test "calculates very first minute average, and inserts at stage one", %{
-    init_series: init_series
-  } do
+  test "calculates very first minute average, and inserts at stage one" do
     sum = 5000
     count = 10
 
-    series = List.replace_at(init_series, 0, {sum, count})
+    series = {0, [{sum, count}, [nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil]]}
 
-    tick_stages = [1]
-
-    assert [
-             {0, 0},
-             [500.0, nil, nil, nil, nil],
-             [nil, nil, nil, nil, nil]
-           ] == Series.tick(series, tick_stages)
+    assert {1,
+            [
+              {0, 0},
+              [500.0, nil, nil, nil, nil],
+              [nil, nil, nil, nil, nil]
+            ]} == Series.tick(series)
   end
 
   test "moves last 5 minute average to next stage" do
     sum = 5000
     count = 10
 
-    tick_stages = [2, 1]
+    series =
+      {5,
+       [
+         {sum, count},
+         [200, 400, 100, 500, 300],
+         [nil, nil, nil, nil, nil]
+       ]}
 
-    series = [
-      {sum, count},
-      [200, 400, 100, 500, 300],
-      [nil, nil, nil, nil, nil]
-    ]
-
-    assert [
-             {0, 0},
-             [500, 200, 400, 100, 500],
-             [300, nil, nil, nil, nil]
-           ] == Series.tick(series, tick_stages)
+    assert {6,
+            [
+              {0, 0},
+              [500, 200, 400, 100, 500],
+              [300, nil, nil, nil, nil]
+            ]} == Series.tick(series)
   end
 
   test "moves last 5 minute average to next stage, when last stage contains data" do
     sum = 5000
     count = 10
 
-    tick_stages = [2, 1]
+    series =
+      {10,
+       [
+         {sum, count},
+         [200, 400, 100, 500, 300],
+         [200, 100, 700, nil, nil]
+       ]}
 
-    series = [
-      {sum, count},
-      [200, 400, 100, 500, 300],
-      [200, 100, 700, nil, nil]
-    ]
-
-    assert [
-             {0, 0},
-             [500, 200, 400, 100, 500],
-             [300, 200, 100, 700, nil]
-           ] == Series.tick(series, tick_stages)
+    assert {11,
+            [
+              {0, 0},
+              [500, 200, 400, 100, 500],
+              [300, 200, 100, 700, nil]
+            ]} == Series.tick(series)
   end
 
   test "pushes out last average" do
     sum = 5000
     count = 10
 
-    tick_stages = [2, 1]
+    series =
+      {10,
+       [
+         {sum, count},
+         [200, 400, 100, 500, 300],
+         [200, 100, 700, 200, 800]
+       ]}
 
-    series = [
-      {sum, count},
-      [200, 400, 100, 500, 300],
-      [200, 100, 700, 200, 800]
-    ]
-
-    assert [
-             {0, 0},
-             [500, 200, 400, 100, 500],
-             [300, 200, 100, 700, 200]
-           ] == Series.tick(series, tick_stages)
+    assert {11,
+            [
+              {0, 0},
+              [500, 200, 400, 100, 500],
+              [300, 200, 100, 700, 200]
+            ]} == Series.tick(series)
   end
 end
